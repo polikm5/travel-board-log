65103b476c69f4502a0249e7aef21fd5184c00f2
chendy <864423284@qq.com>
2025-11-06 21:56:07 +0800

    Modify file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import antfu from "@antfu/eslint-config";
-import withNuxt from "./.nuxt/eslint.config.mjs"
+
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(antfu({
   type: "app",
