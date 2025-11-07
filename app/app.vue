0d7df8d7afe0c5d3559281b662610ab79d69489c
polikm5 <864423284@qq.com>
2025-11-07 17:00:05 +0800

    install tailwindcss & DaisyUI & Icons

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+    <Icon name="uil:github" style="color: black" />
+    <Icon name="uil" style="color: black" />
+  </h1>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: [
-    "@nuxt/eslint",
-  ],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
