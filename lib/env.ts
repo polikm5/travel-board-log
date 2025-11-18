e67ed7526afd3b4d598111083e1642889896c0a2
polikm5 <864423284@qq.com>
2025-11-18 20:42:21 +0800

    Tl 14 (#52)
    * db setup
    
    * 定义schema
    
    * create schema & ready for db connect
    
    * commit github workflow lint.yaml

--- lib/env.ts
+++ lib/env.ts
@@ -4,6 +4,8 @@ import tryParseEnv from "./try-parse";
 
 const EnvSchema = z.object({
   NODE_ENV: z.string(),
+  TURSO_DATABASE_URL: z.string(),
+  TURSO_AUTH_TOKEN: z.string(),
 });
 
 type EnvSchema = z.infer<typeof EnvSchema>;
