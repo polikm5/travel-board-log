898456e0f927e85a71f1935f0b6db6cd77ff2e82
chendy <864423284@qq.com>
2025-11-19 20:13:35 +0800

    auth setup

---
+++ lib/db/schema/auth-schema.ts
@@ -0,0 +1,57 @@
+import { int, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const user = sqliteTable("user", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  email: text().notNull().unique(),
+  emailVerified: integer({ mode: "boolean" })
+    .default(false)
+    .notNull(),
+  image: text(),
+  createdAt: int().notNull().$default(() => Date.now()),
+  updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+});
+
+export const session = sqliteTable("session", {
+  id: int().primaryKey({ autoIncrement: true }),
+  expiresAt: integer({ mode: "timestamp" }).notNull(),
+  token: text().notNull().unique(),
+  createdAt: int().notNull().$default(() => Date.now()),
+  updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+  ipAddress: text(),
+  userAgent: text(),
+  userId: text()
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+});
+
+export const account = sqliteTable("account", {
+  id: int().primaryKey({ autoIncrement: true }),
+  accountId: text().notNull(),
+  providerId: text().notNull(),
+  userId: text()
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  accessToken: text(),
+  refreshToken: text(),
+  idToken: text("id_token"),
+  accessTokenExpiresAt: integer({
+    mode: "timestamp",
+  }),
+  refreshTokenExpiresAt: integer({
+    mode: "timestamp",
+  }),
+  scope: text(),
+  password: text(),
+  createdAt: int().notNull().$default(() => Date.now()),
+  updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+});
+
+export const verification = sqliteTable("verification", {
+  id: int().primaryKey({ autoIncrement: true }),
+  identifier: text().notNull(),
+  value: text().notNull(),
+  expiresAt: integer({ mode: "timestamp_ms" }).notNull(),
+  createdAt: int().notNull().$default(() => Date.now()),
+  updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+});
