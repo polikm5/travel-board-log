c7485c40227d025da557e76e1a6b519ef998c427
chendiyun <864423284@qq.com>
2025-11-26 16:08:30 +0800

    Login by github

---
+++ app/auth-button.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { createAuthClient } from "better-auth/client";
+
+const authClient = createAuthClient();
+
+async function signIn() {
+  await authClient.signIn.social({
+    provider: "github",
+    callbackURL: "/dashboard",
+  });
+}
+</script>
+
+<template>
+  <button class="btn btn-primary" @click="signIn">
+    Sign in
+    <Icon name="tabler:brand-github" size="1.5em" />
+  </button>
+</template>
