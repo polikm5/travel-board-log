3505f64d5daee24926abf4b2b53022c176a8aec4
chendiyun <864423284@qq.com>
2025-11-27 12:58:10 +0800

    feature:log-out

---
+++ app/pages/log-out.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="card container min-h-72 bg-base-300 flex mt-4 flex-col items-center justify-center mx-auto gap-4">
+    <span class="loading loading-spinner loading-xl" />
+  </div>
+</template>
--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -1,20 +1,28 @@
-import { createAuthClient } from "better-auth/client";
+import { createAuthClient } from "better-auth/vue";
 
 const authClient = createAuthClient();
 
 export const useAuthStore = defineStore("useAuthStore", () => {
-  const loading = ref(false);
+  const session = authClient.useSession();
+  const data = computed(() => session.value?.data);
+  const loading = computed(() => session.value.isPending || session.value.isRefetching);
   async function signIn() {
-    loading.value = true;
     await authClient.signIn.social({
       provider: "github",
       callbackURL: "/dashboard",
+      errorCallbackURL: "/error",
     });
-    loading.value = false;
+  }
+
+  async function signOut() {
+    await authClient.signOut();
+    navigateTo("/");
   }
 
   return {
     loading,
+    data,
     signIn,
+    signOut,
   };
 });
