cbf12745b570104e5a919b64b0dc6168928f56b1
polikm5 <864423284@qq.com>
2025-11-27 12:59:51 +0800

    feature:log-out (#56)

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -1,15 +1,37 @@
 <script setup lang="ts">
-const { loading, signIn } = useAuthStore();
+const authStore = useAuthStore();
 </script>
 
 <template>
+  <div v-if="!authStore.loading && authStore.data?.user" class="dropdown dropdown-end">
+    <div
+      tabindex="0"
+      role="button"
+      class="btn m-1"
+    >
+      <div class="avatar">
+        <div v-if="authStore.data.user.image" class="w-6 rounded-full">
+          <img :src="authStore.data.user.image" :alt="authStore.data.user.name">
+        </div>
+      </div>
+      {{ authStore.data.user.name }}
+    </div>
+    <ul tabindex="-1" class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/log-out">
+          Log Out
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
   <button
-    :disabled="loading"
+    v-else
+    :disabled="authStore.loading"
     class="btn btn-primary"
-    @click="signIn"
+    @click="authStore.signIn"
   >
     Sign in
-    <span v-if="loading" class="loading loading-spinner loading-sm" />
+    <span v-if="authStore.loading" class="loading loading-spinner loading-sm" />
     <Icon
       v-else
       name="tabler:brand-github"
