125ad2452dd2c47b2d8957cec1cfb96c996c3745
polikm5 <864423284@qq.com>
2025-12-02 14:12:06 +0800

    feature:sidebar (#61)

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,6 +1,53 @@
 <script setup lang="ts">
+const isSideBarOpen = ref(true);
+onMounted(() => {
+  isSideBarOpen.value = localStorage.getItem("isSideBarOpen") === "true";
+});
+function sideBarState() {
+  isSideBarOpen.value = !isSideBarOpen.value;
+  localStorage.setItem("isSideBarOpen", isSideBarOpen.value.toString());
+}
 </script>
 
 <template>
-  <h1>dashboard page</h1>
+  <div class="flex flex-1">
+    <div class="side-bar bg-base-100 p-2 transition-all duration-300" :class="isSideBarOpen ? 'w-52' : 'w-16'">
+      <div class="justify-self-end hover:bg-base-300" @click="sideBarState">
+        <Icon
+          v-if="isSideBarOpen"
+          name="tabler:arrow-narrow-left"
+          size="2.5em"
+          class="hover:cursor-pointer "
+        />
+        <Icon
+          v-else
+          name="tabler:arrow-narrow-right"
+          size="2.5em"
+          class="hover:cursor-pointer "
+        />
+      </div>
+      <div class="flex flex-col">
+        <SidebarButton
+          label="locations"
+          name="tabler:map"
+          url="/dashboard"
+          :is-show-label="isSideBarOpen"
+        />
+        <SidebarButton
+          label="Add Location"
+          name="tabler:map-plus"
+          url="/dashboard/add-locations"
+          :is-show-label="isSideBarOpen"
+        />
+        <div class="divider" />
+        <SidebarButton
+          label="Log out"
+          name="tabler:logout-2"
+          url="/log-out"
+          :is-show-label="isSideBarOpen"
+        />
+      </div>
+    </div>
+    <div class="flex-1 bg-amber-400" />
+  </div>
 </template>
