b3fc44ca072cc6041ace412921914c027afcda01
chendiyun <864423284@qq.com>
2025-12-04 17:23:24 +0800

    feature:#24

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,5 +1,10 @@
+import type { DrizzleError } from "drizzle-orm";
+
 import db from "~~/lib/db";
 import { InsertLocation, location } from "~~/lib/db/schema";
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+import slugify from "slug";
 
 export default defineEventHandler(async (event) => {
   // 调取接口时 先检查是否已登录
@@ -24,11 +29,47 @@ export default defineEventHandler(async (event) => {
       data,
     }));
   }
+  const locationExist = await db.query.location.findFirst({
+    where: and(
+      eq(location.name, result.data.name),
+      eq(location.userId, event.context.user.id),
+    ),
+  });
+  if (locationExist) {
+    return sendError(event, createError({
+      statusCode: 422,
+      statusMessage: "location name is already exist",
+    }));
+  }
+  let slug = slugify(result.data.name);
 
-  const [inserted] = await db.insert(location).values({
-    ...result.data,
-    slug: result.data.name.replaceAll(" ", "-").toLowerCase(),
-    userId: event.context.user.id,
-  }).returning();
-  return inserted;
+  let existing = !!(await db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  }));
+  while (existing) {
+    const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwsyz", 5);
+    const idSlug = `${slug}-${nanoid()}`;
+    existing = !!(await db.query.location.findFirst({
+      where: eq(location.slug, idSlug),
+    }));
+    if (!existing) {
+      slug = idSlug;
+    }
+  }
+  try {
+    const [inserted] = await db.insert(location).values({
+      ...result.data,
+      slug,
+      userId: event.context.user.id,
+    }).returning();
+    return inserted;
+  }
+  catch (e) {
+    const error = e as DrizzleError;
+    console.error("INSERT ERROR", error.cause);
+    return sendError(event, createError({
+      statusCode: 409,
+      statusMessage: (error?.cause as { message: string }).message || error.message,
+    }));
+  }
 });
