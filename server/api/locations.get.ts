b7c30d80bc8a9ca00b792d2b0a8323af29d1c8fa
chendiyun <864423284@qq.com>
2025-12-05 17:28:26 +0800

    remove delay promise

--- server/api/locations.get.ts
+++ server/api/locations.get.ts
@@ -2,7 +2,6 @@ import { findLocations } from "~~/lib/db/queries/location";
 import defineAuthenciatedEventHandler from "~~/utils/define-authenticated-event-handler";
 
 export default defineAuthenciatedEventHandler(async (event) => {
-  await new Promise(resolve => setTimeout(resolve, 2000));
   const locations = await findLocations(event.context.user.id);
   return locations;
 });
