e42f08946470ba947ba64eb4aa70758593846deb
chendiyun <864423284@qq.com>
2025-12-05 18:56:19 +0800

    feature:add Map

---
+++ app/components/app/map.client.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+const style = computed(() => {
+  return colorMode.value === "dark" ? "/styles/dark.json" : "/styles/light.json";
+});
+const center = [-1.559482, 47.21322];
+const zoom = 8;
+</script>
+
+<template>
+  <MglMap
+    :map-style="style"
+    :center="center"
+    :zoom="zoom"
+  >
+    <MglNavigationControl />
+  </MglMap>
+</template>
+
+<style>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,13 +6,14 @@ import "./lib/env";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode", "@pinia/nuxt", "@vee-validate/nuxt", "nuxt-csurf"],
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode", "@pinia/nuxt", "@vee-validate/nuxt", "nuxt-csurf", "nuxt-maplibre"],
   eslint: {
     config: {
       standalone: false,
     },
   },
   css: ["~/assets/css/main.css"],
+  ssr: false,
   vite: {
     plugins: [tailwindcss()],
   },
