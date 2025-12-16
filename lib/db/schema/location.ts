70e9f9645f9303f1b05f1e98b780ab78c37095f7
polikm5 <864423284@qq.com>
2025-12-16 21:19:40 +0800

    feature:TL-33 (#72) (#73)
    * feature:TL-33
    
    * add github Actions secret key

--- lib/db/schema/location.ts
+++ lib/db/schema/location.ts
@@ -2,7 +2,7 @@ import type z from "zod";
 
 import { relations } from "drizzle-orm";
 import { int, real, sqliteTable, text, unique } from "drizzle-orm/sqlite-core";
-import { createInsertSchema } from "drizzle-zod";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
 import { user } from "./auth-schema";
 import { locationLog } from "./location-log";
@@ -40,6 +40,7 @@ export const InsertLocation = createInsertSchema(location, {
   updatedAt: true,
 });
 
+export const SelectLocation = createSelectSchema(location);
 export type InsertLocation = z.infer<typeof InsertLocation>;
 
 export type LocationType = typeof location.$inferSelect;
