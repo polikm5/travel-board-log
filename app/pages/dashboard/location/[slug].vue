345780b70b79cd7f4da25cda73871a57cdb0d3b7
polikm5 <864423284@qq.com>
2025-12-22 20:45:18 +0800

    TL-35 (#77)

--- app/pages/dashboard/location/[slug].vue
+++ app/pages/dashboard/location/[slug].vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 // import type { SelectLocationWithLogs } from "~~/lib/db/schema";
+import type { FetchError } from "ofetch";
 
 const locationStore = useLocationStore();
 const { currentLocationLog: data, currentLocationError: error, currentLocationStatus: status } = storeToRefs(locationStore);
@@ -9,7 +10,33 @@ const route = useRoute();
 //   lazy: true,
 // });
 // locationStore.currentLocationLog = data.value;
-
+const isOpen = ref(false);
+const deleteLoading = ref(false);
+const deleteError = ref("");
+const loading = computed(() => status.value === "pending" || deleteLoading.value);
+const errorMessage = computed(() => error.value?.statusMessage || deleteError.value);
+function onClose() {
+  isOpen.value = false;
+}
+function openDialog() {
+  (document.activeElement as HTMLAnchorElement).blur();
+  isOpen.value = true;
+}
+async function onConfirm() {
+  try {
+    deleteLoading.value = true;
+    isOpen.value = false;
+    await $fetch(`/api/location/${route.params.slug}`, {
+      method: "delete",
+    });
+    deleteLoading.value = false;
+    navigateTo("/dashboard");
+  }
+  catch (e) {
+    const error = e as FetchError;
+    deleteError.value = error.statusMessage || "An unknown error occurred";
+  }
+}
 onMounted(() => {
   locationStore.refreshLocationLog();
 });
@@ -23,10 +50,46 @@ onBeforeRouteUpdate((to) => {
 
 <template>
   <div class="h-54">
-    <div v-if="status === 'pending'" class="loading loading-xl" />
-    <div v-if="route.name === 'dashboard-location-slug' && status !== 'pending' && data">
+    <div v-if="loading" class="loading loading-xl" />
+    <div v-if="route.name === 'dashboard-location-slug' && !loading && data">
       <div>
         {{ data?.name }}
+        <div class="dropdown">
+          <div
+            tabindex="0"
+            role="button"
+            class="btn m-1 btn-sm p-0"
+          >
+            <Icon name="tabler:dots-vertical" size="20" />
+          </div>
+          <ul tabindex="-1" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <li>
+              <NuxtLink
+                :to="{
+                  name: 'dashboard-location-slug-edit',
+                  params: { slug: data?.slug },
+                }"
+              >
+                <Icon
+                  name="tabler:pencil"
+                  size="16"
+                  class="mr-2"
+                />
+                编辑
+              </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink @click="openDialog">
+                <Icon
+                  name="tabler:trash"
+                  size="16"
+                  class="mr-2"
+                />
+                删除
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
       </div>
       <div>
         {{ data?.description }}
@@ -45,8 +108,18 @@ onBeforeRouteUpdate((to) => {
     <div v-if="route.name !== 'dashboard-location-slug'">
       <NuxtPage />
     </div>
-    <div v-if="status !== 'pending' && error" class="alert alert-error">
-      {{ error.statusMessage }}
+    <div v-if="!loading && errorMessage" class="alert alert-error">
+      {{ errorMessage }}
     </div>
+
+    <AppDialog
+      title="删除"
+      desc="你确定要删除此项吗?"
+      :is-open="isOpen"
+      confirm-label="确定"
+      confirm-class="btn-primary"
+      @on-close="onClose"
+      @on-confirm="onConfirm"
+    />
   </div>
 </template>
